9faf4bb902a5e23416150132cbe5d021e683fcd1
Iurii Beliaev <ib86@duke.edu>
2026-02-16 15:07:51 -0500

    Build: added basic input parser, gitignore

--- src/controller.py
+++ src/controller.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 class Controller:
     def run(self, args=None):
         parsed_input = parse_input(args)
-        logger.info("Parsed input: action=%s, file=%s", parsed_input.action, parsed_input.file_path)
+        logger.info("Parsed input: agent=%s, file=%s", parsed_input.agent, parsed_input.file_path)
 
         agent_name, planned_input = plan(parsed_input)
         logger.info("Planner selected agent: %s", agent_name)
