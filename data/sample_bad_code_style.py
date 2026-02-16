131c1fe73daa24dc1bb770754c1a1c4525b7ceb9
Iurii Beliaev <ib86@duke.edu>
2026-02-16 15:26:30 -0500

    Build: added basic planner, sample bad code style

---
+++ data/sample_bad_code_style.py
@@ -0,0 +1,18 @@
+# Unused import
+# Bad spacing
+# Bad naming
+# Bad comparison
+
+import os,sys
+
+x=10
+y =20
+
+class   myClass:
+    def __init__(self,name):
+        self.name=name
+    def getName( self ):
+        if self.name=="":
+            return "no name"
+        else:
+            return self.name
