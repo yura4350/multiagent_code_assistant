8cb6d6fce1e2c97d32154adeba7719780c58e89e
Iurii Beliaev <ib86@duke.edu>
2026-03-02 01:59:59 -0500

    Refactor: add logger into api caller

--- src/api_caller.py
+++ src/api_caller.py
@@ -1,7 +1,13 @@
+import logging
+
 import requests
 
+logger = logging.getLogger("api_caller")
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(name)s:%(message)s")
+
 # backend_url = "http://localhost:4003"  # Local URL
-backend_url = "http://vcm-52527.vm.duke.edu:4003" # dev server VM URL
+backend_url = "http://vcm-52527.vm.duke.edu:4003"  # dev server VM URL
 payload = {
     "file_content": open("data/sample_bad_idioms.py", encoding="utf-8").read(),
     "file_name": "data/sample_bad_idioms.py",
@@ -11,4 +17,4 @@ payload = {
 r = requests.post(f"{backend_url}/agents/IDIOMS/scan", json=payload, timeout=60)
 r.raise_for_status()
 data = r.json()
-print("issues:", len(data["issues"]))
+logger.info("issues: %d", len(data["issues"]))
