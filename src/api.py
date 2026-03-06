94654ccbbdb80f26ac20457a642bdc55a93116ce
Iurii Beliaev <ib86@duke.edu>
2026-03-06 16:01:39 -0500

    Fix: update default server

--- src/api.py
+++ src/api.py
@@ -4,10 +4,10 @@ import tempfile
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+from src.agents.clean_code_agent import CleanCodeAgent
 from src.agents.code_style_agent import StyleAgent
 from src.agents.idioms_agent import IdiomsAgent
 from src.agents.testing_agent import TestingAgent
-from src.agents.clean_code_agent import CleanCodeAgent
 from src.util.issue import Issue
 from src.util.suggestion import Suggestion
 
