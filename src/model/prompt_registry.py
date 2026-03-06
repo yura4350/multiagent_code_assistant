770053a826f60e824047d0ac6830919eeadbb821
Iurii Beliaev <ib86@duke.edu>
2026-03-06 03:15:59 -0500

    Add prompts for testing and put them into the prompt registry

--- src/model/prompt_registry.py
+++ src/model/prompt_registry.py
@@ -9,9 +9,11 @@ class PromptRegistry:
         )
 
         self._name_to_file = {
-            "idioms.generate_suggestions": "idioms/generate_suggestions.txt",
             "idioms.scan": "idioms/scan.txt",
+            "idioms.generate_suggestions": "idioms/generate_suggestions.txt",
             "idioms.apply": "idioms/apply.txt",
+            "testing.scan": "testing/scan.txt",
+            "testing.generate_suggestions": "testing/generate_suggestions.txt",
         }
 
     def load(self, prompt_name: str) -> str:
